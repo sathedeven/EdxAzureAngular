7622d075f5e1b8f40b1cc203cabe4e0317baa253
deven <sathedeven@gmail.com>
2018-08-09 12:34:23 +0530

    azure ad

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { UserService } from './../common/services/user.service';
+import { User } from './../common/models/user';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    user:User;
+    constructor(private userService : UserService){
+    }
+
+    ngOnInit() : void{
+        this.userService.getUser().subscribe(user=>this.user = user);
+    }
 }
+
